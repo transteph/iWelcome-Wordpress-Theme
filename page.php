37a5973250af43c281ca9dc3bc566805832498f4
Stephanie <stran40@myseneca.ca>
2017-07-24 20:37:10 -0400

    some filter and hover effects set on images

--- page.php
+++ page.php
@@ -14,55 +14,40 @@
 get_header(); ?>
 
 	<div class="site-wrapper">
-		<div class="site-wrapper-inner">
 			<main id="main" class="site-main" role="main">
 <div class="post" id="post-<?php the_ID(); ?>">
-		<div class="inner cover">
-		
-<ul class="front-page-posts">
+	
 		<?php
              $args = array( 'category__in' => 2, 3  );
               $query = new WP_Query( $args ); 
 ?>
 
 	<?php if ( $query->have_posts() ) : ?>
-
-		<div class="col-md-12">
-				<div class="row">
 					<div class="grid-outer">
 						
 						<?php while ( $query->have_posts() ) : $query->the_post(); ?>
-							<li>
 								<div class="overlay">
 									<!--  Image -->  
 									<?php if ( has_post_thumbnail()) : ?>
-										
+										<div class="img-container">
 											<a class="post-title-link" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
-												<img src="<?php the_post_thumbnail_url(); ?>"/>
+												<img class="blur" src="<?php the_post_thumbnail_url(); ?>"/>
 													<span class="post-title">  <?php the_title(); ?></span>
 											</a>
-
+										</div>
 										<?php else :?>
 											<a class="post-title-link" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
 												<?php the_title(); ?>
 										</a>
 									<?php endif; ?>
 									</div>	
-							</li>
 					
 
 						<?php endwhile; ?><!-- end of the loop -->
-						<!-- last box -->
-						<li>
-							<h3>iWelcome Refugees and Migrants</h3>
-						</li>
 					</div><!-- .grid-outer -->
-				</div><!-- .row -->
-		</div><!-- .col-md-12 -->
 	<?php wp_reset_postdata(); ?>
 	<?php endif; ?>
-</ul>
-</div><!-- .inner .cover -->
+				</div>
 </main><!-- #main -->
 </div>	<!-- #primary -->
 </div><!-- .wrap -->
