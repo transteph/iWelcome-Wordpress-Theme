f2c7b3479b9aac417b5e8cba991e7538ee7f3677
Stephanie <stran40@myseneca.ca>
2017-08-23 22:52:56 -0400

    Fixed spelling mistake in description. Took out top comment.

--- header.php
+++ header.php
@@ -1,14 +1,4 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- */
 
-?>
 	<!DOCTYPE html>
 	<html <?php language_attributes(); ?> class="no-js no-svg" xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#" xmlns:fb="http://www.facebook.com/2008/fbml">
 
@@ -17,11 +7,11 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta property="og:updated_time" content="<?=time()?>" />
 		<meta attribute="author" content="Stephanie Tran, Amnesty International Toronto Organization" />
-		<meta attribute="description" content="iWelcome Refugees - an Amnesty International campaign welcoming refugee and migrants.">
+		<meta attribute="description" content="iWelcome Refugees - an Amnesty International campaign welcoming refugees and migrants.">
 		<meta attribute="keywords" content="AITO, Amnesty, Amnesty International, Toronto, human rights, refugee rights"
 		/>
 
-		<meta property="og:url" content="http://aito.ca/iWelcome" />
+		<meta property="og:url" content="https://aito.ca/iWelcome" />
 		<meta property="og:type" content="website" />
 		<meta property="og:title" content="iWelcome Refugees" />
 		<meta property="og:description" content="iWelcome Refugees - an Amnesty International campaign welcoming refugee and migrants."
